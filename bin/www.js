907ccecd5e591f2942b19087eee1d9410e3eea19
xrocketmanx <xrocketmanx007@gmail.com>
2017-04-21 22:43:00 +0300

    fixed port

--- bin/www.js
+++ bin/www.js
@@ -2,7 +2,7 @@ var app = require('../app');
 var socketServer = require('../web-streaming');
 
 var PORT = process.env['PORT'] || 3000;
-var SOCKET_PORT = PORT + 1;
+var SOCKET_PORT = 8080;
 
 app.listen(PORT, function() {
     console.log('listening on ' + PORT);
